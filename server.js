68845f43267d7427e9ac60a784b60fb0eb6d3176
sampsonli <740056710@qq.com>
2017-07-04 15:16:44 +0800

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lichun on 2017/6/27.
+ */
+const path = require('path')
+const express = require('express')
+const app = express()
+app.use(express.static(path.join(__dirname, 'static')))
+const port = 80
+app.listen(port, () => {
+    console.log(`Listening at ${port}`)
+})
